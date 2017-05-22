4998ca09dcdc616d9cafb0eabd60cf79ea9f630e
envy6 <buquerin.kevin@gmail.com>
2017-05-22 17:51:21 +0200

    Final improvments to print() Functions. Ready to publish.

--- backpacker.py
+++ backpacker.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 '''
-17.05.2017
+22.05.2017
 Backpacker for it-talents code competition May 2017
 AUTHOR: Kevin Gomez Buquerin
 ALL RIGHTS RESERVED
 '''
-# GLOBAL VARIABLES
+
+# Colors for prints
+class ansi:
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    YELLOW = '\033[93m'
+    END = '\033[0m'
 
 '''
 Function: user_interaction_items
@@ -14,18 +21,21 @@ Return: items - items provided by the user
 This function gets the user input and puts every item into a list.
 '''
 def user_interaction_items():
-    print("[+] List an item like this: WEIGHT,VALUE,'ITEMNAME'")
-    print("[+] With ITEMNAME as the name of the item (string, with single quotes), WEIGHT as the weight of ONE item (number in kg) and VALUE as the value of the corresponding item in EURO.")
-    print("[+] Press ENTER if you are finished with entering items.")
+    print("{}[+]{} List an item like this: {}WEIGHT,VALUE,'ITEMNAME'{}".format(ansi.GREEN, ansi.END, ansi.BLUE, ansi.END))
+    print("{}[+]{} With {}ITEMNAME{} as the name of the item (string, with single quotes), {}WEIGHT{} as the weight of ONE item (number in g) and {}VALUE{} as the value of the corresponding item in EURO.".format(ansi.GREEN, ansi.END, ansi.BLUE, ansi.END, ansi.BLUE, ansi.END, ansi.BLUE, ansi.END))
+    print("{}[+]{} Example: {}12,3,ball 20,10,smartphone{} ...".format(ansi.GREEN, ansi.END, ansi.YELLOW, ansi.END))
+    print("\n")
+    print("{}[+]{} Press {}ENTER{} if you are finished with entering items.".format(ansi.GREEN, ansi.END, ansi.BLUE, ansi.END))
+
     items = []
-    i = 0
     while 1:
-        i += 1
         try:
-            user_input = input("[+] Please add a new item: ")
+            user_input = input("{}[+]{} Please add a new item: ".format(ansi.GREEN, ansi.END))
         except:
             break
-        print("[+] Item " + str(user_input) +  " successfully added!")
+        print("{}[+]{} Item {}".format(ansi.GREEN, ansi.END, ansi.BLUE) + str(user_input) +  "{} successfully added!".format(ansi.END))
+        print("\n")
+
         items.append(user_input)
     return items
 
@@ -39,6 +49,8 @@ This function solves the Knapsack problem.
 Check out: https://en.wikipedia.org/wiki/Knapsack_problem
 '''
 def backpack_logic(items, max_weight):
+    
+    # define Weight, Value and Name of the items
     def itemWeight(item): return item[0]
     def itemValue(item): return item[1]
     def itemName(item): return item[2]
@@ -81,29 +93,43 @@ def get_weight_sum(items_list, items):
             res += items[i][0]
     return res
 
+'''
+Function: main
+Paramters: none
+Return: none
+Main Function for the Python Script.
+'''
 if __name__ == "__main__":
-    print("[*] Welcome to the backpacker.")
+    print("{}[*]{} Welcome to the backpacker.".format(ansi.BLUE, ansi.END))
+
+    # check for valid user input
     while True:
         try:
-            max_weight = input("[+] What's the maximal weight for your backpack? ")
+            max_weight = input("{}[+]{} What's the maximal weight for your backpack? ".format(ansi.GREEN, ansi.END))
+
+            # check if the user provided an integer
             if isinstance(max_weight, int):
-                print("[+] A maximum weight of " + str(max_weight) + " for your backpack, successfully added!")
+                print("{}[+]{} A maximum weight of {}".format(ansi.GREEN, ansi.END, ansi.BLUE) + str(max_weight) + "{} for your backpack, successfully added!".format(ansi.END))
                 break
             else:
-                print("[-] Please provide a number for the maximum weight of your backpack. Try again!")
+                print("{}[-]{} Please provide a number for the maximum weight of your backpack. Try again!".format(ansi.RED, ansi.END))
+                print("\n")
         except:
-                print("[-] Please provide a number for the maximum weight of your backpack. Try again!")
-
-    #max_weight = 50  # debug value
-    print("[+] Maximal weight for the backpack is set to " + str(max_weight) + ".")
-
+                print("{}[-]{} Please provide a number for the maximum weight of your backpack. Try again!".format(ansi.RED, ansi.END))
+                print("\n")
+    print("\n")
+    
+    # get the items from the user
     items = user_interaction_items()
-    print("[+] You added the followeing items: " + str(items) + ".")
+    print("\n")
 
-    print("[+] Start to calculate the perfect way to pack your backpack!")
+    print("{}[+]{} You added the followeing items: {}".format(ansi.GREEN, ansi.END, ansi.BLUE) + str(items) + "{}.".format(ansi.END))
+    print("\n")
+
+    print("{}[+]{} Start to calculate the perfect way to pack your backpack...".format(ansi.GREEN, ansi.END))
     items_list, items = backpack_logic(items, max_weight)
-    print("[+] Pack the following items: " + str(items_list) + ".")
+    
+    print("{}[+]{} Pack the following items: {}".format(ansi.GREEN, ansi.END, ansi.BLUE) + str(items_list) + "{}.".format(ansi.END))
     calculated_weight = get_weight_sum(items_list, items)
-    print("[+] The optimal weight for the provided values is: " + str(calculated_weight) + ".")
-
-    # TODO color the console output
+    
+    print("{}[+]{} The optimal weight for the provided values is: {}".format(ansi.GREEN, ansi.END, ansi.BLUE) + str(calculated_weight) + "{}.".format(ansi.END))
